dbc7e61cf5a2435a3edf0411e58a31dd9f4edf82
moobin <moobinislam3@gmail.com>
2025-01-04 12:58:39 +0600

    aa

--- lib/dart.dart
+++ lib/dart.dart
@@ -1,1 +1,13 @@
-import 'package:dart_consol_appliacation/polymarphrison/parson.dart';import 'package:dart_consol_appliacation/polymarphrison/student.dart';import 'package:dart_consol_appliacation/polymarphrison/teacher.dart';void main() {  Parson p = Parson();  p.display();  p = Teacher();  p.display();  p = Student();  p.display();}
+import 'package:dart_consol_appliacation/polymarphrison/parson.dart';
+import 'package:dart_consol_appliacation/polymarphrison/student.dart';
+import 'package:dart_consol_appliacation/polymarphrison/teacher.dart';
+
+void main() {
+  Parson p = Parson();
+  p.display();
+  p = Teacher();
+  p.display();
+  p = Student();
+  //name 
+  p.display();
+}
